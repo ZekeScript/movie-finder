b02cb89504fd3f0236e85e6d05d7db567fcde4f6
ZekeScript <estiga27@gmail.com>
2023-09-27 23:13:40 -0300

    add a mock for types and format the search style with it

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import './App.css'
+import responseMovies from './mocks/with-results.json'
+// import withoutResults from './mocks/no-results.json'
 
 // [x] Add css framework classless
 // [x] Add an input with a search button
+// [x] Add mocks for type manager easily
+// [ ] List the movies found and display the title, year, and poster
 
 function App () {
+  const movies = responseMovies.Search
+  const hasMovies = movies?.length > 0
   return (
     <>
       <header>
@@ -13,6 +19,28 @@ function App () {
           <button type='submit'>Search</button>
         </form>
       </header>
+
+      <main>
+        {
+          hasMovies
+            ? (
+              <ul>
+                {
+                movies.map(movie => (
+                  <li key={movie.imdbID}>
+                    <h3>{movie.Title}</h3>
+                    <p>{movie.Year}</p>
+                    <img src={movie.Poster} alt={movie.Title} />
+                  </li>
+                ))
+              }
+              </ul>
+              )
+            : (
+              <p>No se encontraron peliculas que coincidan con la busqueda</p>
+              )
+        }
+      </main>
     </>
   )
 }
